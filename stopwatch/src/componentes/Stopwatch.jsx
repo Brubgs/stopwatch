f0d00d9beca506a82e5b5a4003e4bcc6d6b3b0bc
Bruna Borges <bruna.lborges.matias@gmail.com>
2024-10-29 20:21:13 -0300

    javacript

--- stopwatch/src/componentes/Stopwatch.jsx
+++ stopwatch/src/componentes/Stopwatch.jsx
@@ -8,24 +8,46 @@ function Stopwatch(){
     const iniciarTempo = useRef(0)
 
     useEffect(() =>{
+        if(rodando){
+            intervalo.current = setInterval(() => {
+                setElapsedTime(Date.now()-iniciarTempo.current)
+            }, 10)
+        }
 
+        return () => {
+            clearInterval(intervalo.current)
+        }
     },[rodando])
 
-function Iniciar(){
+    function Iniciar(){
+        setRodando(true)
+        iniciarTempo.current = Date.now() - elapsedTime
+    }
+    function Encerrar(){
+        setRodando(false)
+    }
+    function Reiniciar(){
+        setElapsedTime(0)
+        setRodando(false)
+    }
+    function Formatar(){
 
-}
-function Encerrar(){
+        let horas = Math.floor(elapsedTime / (1000 * 60 * 60))
+        let minutos = Math.floor(elapsedTime / (1000 * 60) % 60)
+        let segundos = Math.floor(elapsedTime / (1000) % 60)
+        let milisegundos = Math.floor(elapsedTime % (1000) / 10)
 
-}
-function Reiniciar(){
+        horas = String(horas).padStart(2, "0")
+        minutos = String(minutos).padStart(2, "0")
+        segundos = String(segundos).padStart(2, "0")
+        milisegundos = String(milisegundos).padStart(2, "0")
 
-}
-function Formatar(){
-    return `00:00:00`
-}
+        return `${horas}:${minutos}:${segundos}:${milisegundos}`
+    }
 
     return(
         <>
+        <h1>Stopwatch</h1>
             <div className="stopwatch">
                 <div className="display">{Formatar()}</div>
                 <div className="controles">
@@ -34,6 +56,7 @@ function Formatar(){
                     <button className='botaoResetar' onClick={Reiniciar}>Reiniciar</button>
                 </div>
             </div>
+            <h5>Desenvolvido por Bruna Borges <br />Thanks BroCode S2</h5>
         </>
     )
 }
